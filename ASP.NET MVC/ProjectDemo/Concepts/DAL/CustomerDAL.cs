fe5ad31f0b024c95fcb4bbbf45cecdc46d2dcf9f
Omkar Gorde <gordeaumkar@outlook.com>
2023-06-26 16:11:16 +0530

    EF and AJAX

---
+++ ASP.NET MVC/ProjectDemo/Concepts/DAL/CustomerDAL.cs
@@ -0,0 +1,21 @@
+﻿using Concepts.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Concepts.DAL
+{
+    //Here a mistake is made while creating an id, we have marked CustomerId as key but we are also taking it as input from UI but in DB treating as Identity column
+    public class CustomerDAL: DbContext
+    {
+        public DbSet<Customer> dbSet { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customer>().ToTable("efCustomer"); // we should provide sql table name here
+        }
+    }
+}
--- ASP.NET MVC/ProjectDemo/Concepts/Models/Customer.cs
+++ ASP.NET MVC/ProjectDemo/Concepts/Models/Customer.cs
@@ -6,8 +6,10 @@ using System.Web;
 
 namespace Concepts.Models
 {
+    //Here a mistake is made while creating an id, we have marked CustomerId as key but we are also taking it as input from UI but in DB treating as Identity column
     public class Customer
     {
+        [Key]         //This is required attribute for ef
         [Required]
         [RegularExpression("^[0-9]{4,4}$")]
         public int CustomerId { get; set; }
